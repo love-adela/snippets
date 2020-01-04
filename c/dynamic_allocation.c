6ddcfabd64effebc474387d91bf5e07898faeb40
Adela Chung <love.adelar@gmail.com>
2020-01-04 19:52:34 +0900

    Update dynamic_allocation.c

--- c/dynamic_allocation.c
+++ c/dynamic_allocation.c
@@ -10,11 +10,12 @@ int main() {
   strcpy(str, "adela");
   printf("String = %s, Address = %p\n", str, str);
 
-  /* Reallocating memory: 크기를 늘리거나 줄인다. */
+  /* Reallocating memory: 크기를 늘리거나 줄입니다. */
   str = (char *) realloc(str, 17);
   strcat(str, ".love");
   printf("String = %s, Address = %p\n", str, str);
 
   free(str);
+  /* 프로그램이 꺼질 때까지 메모리 누수가 발생한다. 따라서 더이상 쓰지 않을 때 바로 deallocate을 해야 합니다.*/
   return (0);
 }
