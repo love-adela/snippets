591a89cab070de78a0c018605d1f5649de69f181
love-adelar <love.adelar@gmail.com>
2020-07-14 22:20:06 +0900

    시계열 달팽이 수열 잘못된 부분 수정

--- python/snail-clock-wise.py
+++ python/snail-clock-wise.py
@@ -13,20 +13,30 @@ C, R = map(int, input().split())
 grid = [[0]* C for _ in range(R)]
 
 count, offset = 0, 0
+max_size = C*R
 
 while R > 0 and C > 0:
     for i in range(offset+R-1, offset-1, -1):
+        if count >= max_size:
+            break
         count += 1
         grid[i][offset] = count
+
     for j in range(offset+1, offset+C):
+        if count >= max_size:
+            break
         count += 1
         grid[offset][j] = count
 
     for i in range(offset+1, offset+R):
+        if count >= max_size:
+            break
         count += 1
         grid[i][offset+C-1] = count
 
     for j in range(offset+C-2, offset, -1):
+        if count >= max_size:
+            break
         count += 1
         grid[offset+R-1][j] = count
 
