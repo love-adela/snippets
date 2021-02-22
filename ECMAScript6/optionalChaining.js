2ad96df7d9ea3532c1b8d6f4d346944204098b99
Adela Chung <love.adelar@gmail.com>
2021-02-23 00:24:59 +0900

    Add an expression - Optional chaining operator

---
+++ ECMAScript6/optionalChaining.js
@@ -0,0 +1,14 @@
+// Expressions: Optional chaining operator
+const adventurer = {
+  name: 'Alice',
+  cat: {
+    name: 'Dinah'
+  }
+};
+
+const dogName = adventurer.dog?.name;
+console.log(dogName);
+// expected output: undefined
+
+console.log(adventurer.someNonExistentMethod?.());
+// expected output: undefined
